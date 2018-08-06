dc6d259c94f02c52fa2d90fa0704915718fddbc2
dimaszsiddik <zaparsiddik@gmail.com>
2018-08-06 14:25:51 +0700

    tambah tables

--- components/controllers/tables.controller.js
+++ components/controllers/tables.controller.js
@@ -13,21 +13,42 @@ module.exports = exports = function (server) {
             if (err) throw err;
             dbo = db.db(config.dbname);
             dbo.collection(name)
-                .aggregate([
-                    { $lookup: { from: 'reservations', localField: '_id', foreignField: 'tableId', as: 'reservation' } },
-                    { $unwind: { path: '$reservation', "preserveNullAndEmptyArrays": true } },
-                    { $lookup: { from: 'orders', localField: 'reservation._id', foreignField: 'reservationId', as: 'orders' } },
-                    {
-                        $project: {
-                            '_id': 1,
-                            'code': 1,
-                            'description': 1,
-                            'reservation._id': 1,
-                            'reservation.guest': 1,
-                            'orders': 1
-                        }
-                    }
-                ]).toArray(function (err, response) {
+            .aggregate([
+                { "$lookup": {
+                  "from": "reservations",
+                  "localField": "_id",
+                  "foreignField": "tableId",
+                  "as": "reservation"
+                }},
+                { "$unwind": { "path": '$reservation', 'preserveNullAndEmptyArrays': true }},
+                { "$lookup": {
+                  "from": "orders",
+                  "localField": "reservation._id",
+                  "foreignField": "reservationId",
+                  "as": "orders",
+                }},
+                { "$unwind": { "path": '$orders', 'preserveNullAndEmptyArrays': true }},
+                { "$lookup": {
+                  "from": "products",
+                  "localField": "orders.productId",
+                  "foreignField": "_id",
+                  "as": "orders.products"
+                }},
+                { "$unwind": { "path": '$orders.products', 'preserveNullAndEmptyArrays': true }},
+                { "$addFields": {
+                  "orders.intial": "$orders.products.initial",
+                  "orders.name": "$orders.products.name"
+                }},
+                { "$group": {
+                  "_id": "$_id",
+                  "Code": { "$first": "$code" },
+                  "Seat": { "$first": "$seat" },
+                  "description": { "$first": "$description" },
+                  "reservation": { "$first": "$reservation" },
+                  "orders": { "$push": "$orders" }
+                }},
+                { "$project": { "orders.products": 0 }}
+              ]).toArray(function (err, response) {
                     if (err) throw err;
                     res.send(200, response);
                     db.close();
